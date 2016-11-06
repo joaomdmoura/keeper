62595b62f34e2f65908427fe095342431a8b94e3
João Moura <joaomdmoura@gmail.com>
2016-11-06 03:24:50 -0200

    Adding travis integration

--- config/config.exs
+++ config/config.exs
@@ -1,2 +1,11 @@
 use Mix.Config
-import_config "#{Mix.env}.exs"
+
+# Disable colors during tests.
+config :logger, :console, colors: [enabled: false]
+
+# Use higher stacktrace depth.
+config :phoenix, :stacktrace_depth, 20
+
+# Make comeonin faster by reducing its encryption power
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
