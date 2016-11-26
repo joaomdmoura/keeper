0bc170b4f5bb7cda5e2ea01cb1cb397c98ca7743
João Moura <joaomdmoura@gmail.com>
2016-11-26 02:45:33 -0200

    refactoring and adding new test

--- test/mix/tasks/keeper.install_test.exs
+++ test/mix/tasks/keeper.install_test.exs
@@ -1,19 +1,34 @@
 defmodule Mix.Tasks.Keeper.InstallTest do
   use ExUnit.Case
 
+  @web_path "web/"
+  @model_path "web/models"
+
   setup do
     Mix.Task.clear
     :ok
   end
 
   test "install task should work if resource name and its plural name are send" do
-    assert Mix.Tasks.Keeper.Install.run ["User", "users"]
+    assert successful_install
 
-    on_exit fn ->
-      # Cleaning support test files
-      options = ["-rf", "web/"]
-      {_stdout, 0} = System.cmd("rm", options)
+    on_exit &clean_support_files/0
+  end
+
+  test "successful installation should result on a model module" do
+    successful_install
+
+    # Check if model module was copied correctly
+    {:ok, files} = File.ls @model_path
+    assert files == ["user.ex"]
+
+    # Check for the correct module name
+    Enum.each files, fn(fname) ->
+      {:ok, contents} = Path.join(@model_path, fname) |> File.read
+      assert contents =~ ~r/defmodule\s*[A-Za-z0-9].+\.User/
     end
+
+    on_exit &clean_support_files/0
   end
 
   test "resource name must be passed as an argument" do
@@ -38,5 +53,13 @@ defmodule Mix.Tasks.Keeper.InstallTest do
     assert_raise Mix.Error, fn ->
       Mix.Tasks.Keeper.Install.run ["User", "User"]
     end
+  end
+
+  defp successful_install, do: Mix.Tasks.Keeper.Install.run ["User", "users"]
+
+  defp clean_support_files do
+    # Cleaning support test files
+    options = ["-rf", @web_path]
+    {_stdout, 0} = System.cmd("rm", options)
   end
 end
